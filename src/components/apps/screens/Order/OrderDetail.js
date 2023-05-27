68e20b085100f7f4600fdd37fc8899b6b892ca78
tinpqps19513 <tinpqps19513@gmail.com>
2023-05-27 21:49:48 +0700

    Cập nhật thêm danh sách sản phẩm vào trang chi tiết đơn hàng
    dữ liệu set cứng

--- src/components/apps/screens/Order/OrderDetail.js
+++ src/components/apps/screens/Order/OrderDetail.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from 'react-native'
 import React from 'react'
 
 const OrderDetail = (props) => {
@@ -7,6 +7,33 @@ const OrderDetail = (props) => {
   const handleBackPress = () => {
     navigation.navigate('Order');
   };
+
+  const data = [
+    { id: 'LT238562312', img: '', productName: 'Macbook Pro 2022', quantity: '03', price: '$150', color: 'Silver' },
+    { id: 'LT238562312', img: '', productName: 'Macbook Pro 2022', quantity: '03', price: '$150', color: 'Silver' },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between', width: '100%', borderBottomColor:'#000', borderBottomWidth:0.5 }}>
+      <View style={{ width: 80, height: 80 }}>
+        <Image
+          style={{ width: '100%', height: '100%' }}
+          source={require('../../../../assets/images/Iphone14.png')} />
+      </View>
+      <View>
+        <Text style={[styles.text1, { color: '#242424' }]}>{item.productName}</Text>
+        <Text style={[styles.text2, { color: '#808080' }]}>Color: {item.color}</Text>
+        <Text style={[styles.text2, { color: '#808080' }]}>{item.price}</Text>
+      </View>
+      <View style={{ justifyContent: 'space-between' }}>
+        <View></View>
+        <TouchableOpacity style={{ backgroundColor: 'black', alignItems: 'center', justifyContent: 'center', padding: 5, borderRadius: 5 }}>
+          <Text style={[styles.text2, { color: '#fff' }]}>Review</Text>
+        </TouchableOpacity>
+        <Text style={[styles.text2, { color: '#808080' }]}>Quantity: {item.quantity}</Text>
+      </View>
+    </View>
+  );
   return (
     <View style={styles.container}>
       <View style={styles.headerBox}>
@@ -27,18 +54,18 @@ const OrderDetail = (props) => {
           <Text style={[styles.text2, { color: '#808080' }]}>20/03/2020</Text>
         </View>
         <View style={{ backgroundColor: '#F0F0F0', height: 1, width: '100%' }}></View>
-        <View style={[styles.functionBox, { flexDirection: 'column', alignItems: 'flex-start', borderBottomRightRadius: 8, paddingBottom:13 }]}>
-          <View style={{ flexDirection: 'row', marginTop:15 }}>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Total: </Text>
-            <Text style={[styles.text1, { color: '#303030', flex:1 }]}>$150</Text>
+        <View style={[styles.functionBox, { flexDirection: 'column', alignItems: 'flex-start', borderBottomRightRadius: 8, paddingBottom: 13 }]}>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Total: </Text>
+            <Text style={[styles.text1, { color: '#303030', flex: 1 }]}>$150</Text>
           </View>
-          <View style={{ flexDirection: 'row', marginTop:15 }}>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Payments: </Text>
-            <Text style={[styles.text1, { color: '#242424', flex:1 }]}>Cash</Text>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Payments: </Text>
+            <Text style={[styles.text1, { color: '#242424', flex: 1 }]}>Cash</Text>
           </View>
-          <View style={{ flexDirection: 'row', marginTop:15 }}>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Status: </Text>
-            <Text style={[styles.text1, { color: '#27AE60', flex:1 }]}>Success</Text>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Status: </Text>
+            <Text style={[styles.text1, { color: '#27AE60', flex: 1 }]}>Success</Text>
           </View>
         </View>
       </View>
@@ -48,20 +75,30 @@ const OrderDetail = (props) => {
           <Text style={[styles.text1, { color: '#242424' }]}>Customer Information</Text>
         </View>
         <View style={{ backgroundColor: '#F0F0F0', height: 1, width: '100%' }}></View>
-        <View style={[styles.functionBox, { flexDirection: 'column', alignItems: 'flex-start', borderBottomRightRadius: 8, paddingBottom:13 }]}>
-          <View style={{ flexDirection: 'row', marginTop:15 }}>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Name: </Text>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Phạm Quốc Tín</Text>
+        <View style={[styles.functionBox, { flexDirection: 'column', alignItems: 'flex-start', borderBottomRightRadius: 8, paddingBottom: 13 }]}>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Name: </Text>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Phạm Quốc Tín</Text>
           </View>
-          <View style={{ flexDirection: 'row', marginTop:15 }}>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Phone number: </Text>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>113</Text>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Phone number: </Text>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>113</Text>
           </View>
-          <View style={{ flexDirection: 'row', marginTop:15 }}>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>Address: </Text>
-            <Text style={[styles.text1, { color: '#808080', flex:1 }]}>FPT</Text>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>Address: </Text>
+            <Text style={[styles.text1, { color: '#808080', flex: 1 }]}>FPT</Text>
           </View>
         </View>
+      </View>
+
+      <View style={[styles.functionBox, { flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', marginTop: 25, padding: 20 }]}>
+        <Text style={[styles.text1, { color: '#242424' }]}>List Product</Text>
+        <FlatList
+          style={{ width: '100%' }}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </View>
   )
